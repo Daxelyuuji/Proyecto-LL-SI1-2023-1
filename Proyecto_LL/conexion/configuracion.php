2f241974da5443b37e80815e29054fc7c8ee0143
Torres Allccarima, Roy Axel <u20238006@utp.edu.pe>
2023-05-21 02:00:34 -0500

    oks

--- Proyecto_LL/conexion/configuracion.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set("America/Lima");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");   # Usuario root
-define("DB_PASSWORD", "");   # Sin contraseña
-define("DB_NAME", "llauce");   # Nombre de la base de datos
-define("DB_PORT", "3306");   # Puerto 3306 - MySQL, 3307 - MariaBD
--- Proyecto_LL/principales/validar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if($_SESSION["acceso"] != "E14007a") {
-        session_destroy();
-        header("location: login.php?e=access");
-    }
-?>
